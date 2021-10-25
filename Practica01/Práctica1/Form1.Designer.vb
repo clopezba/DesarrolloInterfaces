c9db05e5e2ccbefbe5eb1bb3726ef1dcf0c8a76f
Cristina <200101@iessierradeguara.com>
2021-10-25 19:39:49 +0200

    Practica01

---
+++ Practica01/Práctica1/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BtnPulsa = New System.Windows.Forms.Button()
+        Me.LblDobleClick = New System.Windows.Forms.Label()
+        Me.PnlRbt = New System.Windows.Forms.Panel()
+        Me.RbtOpc4 = New System.Windows.Forms.RadioButton()
+        Me.RbtOpc3 = New System.Windows.Forms.RadioButton()
+        Me.RbtOpc2 = New System.Windows.Forms.RadioButton()
+        Me.RbtOpc1 = New System.Windows.Forms.RadioButton()
+        Me.BtnNum = New System.Windows.Forms.Button()
+        Me.Txt1 = New System.Windows.Forms.TextBox()
+        Me.Txt2 = New System.Windows.Forms.TextBox()
+        Me.BtnTexto = New System.Windows.Forms.Button()
+        Me.PnlRbt.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'BtnPulsa
+        '
+        Me.BtnPulsa.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnPulsa.Location = New System.Drawing.Point(69, 23)
+        Me.BtnPulsa.Name = "BtnPulsa"
+        Me.BtnPulsa.Size = New System.Drawing.Size(101, 36)
+        Me.BtnPulsa.TabIndex = 0
+        Me.BtnPulsa.Text = "Pulsa"
+        Me.BtnPulsa.UseVisualStyleBackColor = True
+        '
+        'LblDobleClick
+        '
+        Me.LblDobleClick.AutoSize = True
+        Me.LblDobleClick.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.LblDobleClick.Location = New System.Drawing.Point(69, 83)
+        Me.LblDobleClick.Name = "LblDobleClick"
+        Me.LblDobleClick.Size = New System.Drawing.Size(84, 21)
+        Me.LblDobleClick.TabIndex = 1
+        Me.LblDobleClick.Text = "DobleClick"
+        '
+        'PnlRbt
+        '
+        Me.PnlRbt.Controls.Add(Me.RbtOpc4)
+        Me.PnlRbt.Controls.Add(Me.RbtOpc3)
+        Me.PnlRbt.Controls.Add(Me.RbtOpc2)
+        Me.PnlRbt.Controls.Add(Me.RbtOpc1)
+        Me.PnlRbt.Location = New System.Drawing.Point(69, 121)
+        Me.PnlRbt.Name = "PnlRbt"
+        Me.PnlRbt.Size = New System.Drawing.Size(134, 146)
+        Me.PnlRbt.TabIndex = 2
+        '
+        'RbtOpc4
+        '
+        Me.RbtOpc4.AutoSize = True
+        Me.RbtOpc4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.RbtOpc4.Location = New System.Drawing.Point(20, 104)
+        Me.RbtOpc4.Name = "RbtOpc4"
+        Me.RbtOpc4.Size = New System.Drawing.Size(91, 25)
+        Me.RbtOpc4.TabIndex = 0
+        Me.RbtOpc4.TabStop = True
+        Me.RbtOpc4.Text = "Opción 4"
+        Me.RbtOpc4.UseVisualStyleBackColor = True
+        '
+        'RbtOpc3
+        '
+        Me.RbtOpc3.AutoSize = True
+        Me.RbtOpc3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.RbtOpc3.Location = New System.Drawing.Point(20, 74)
+        Me.RbtOpc3.Name = "RbtOpc3"
+        Me.RbtOpc3.Size = New System.Drawing.Size(91, 25)
+        Me.RbtOpc3.TabIndex = 0
+        Me.RbtOpc3.TabStop = True
+        Me.RbtOpc3.Text = "Opción 3"
+        Me.RbtOpc3.UseVisualStyleBackColor = True
+        '
+        'RbtOpc2
+        '
+        Me.RbtOpc2.AutoSize = True
+        Me.RbtOpc2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.RbtOpc2.Location = New System.Drawing.Point(20, 44)
+        Me.RbtOpc2.Name = "RbtOpc2"
+        Me.RbtOpc2.Size = New System.Drawing.Size(91, 25)
+        Me.RbtOpc2.TabIndex = 0
+        Me.RbtOpc2.TabStop = True
+        Me.RbtOpc2.Text = "Opción 2"
+        Me.RbtOpc2.UseVisualStyleBackColor = True
+        '
+        'RbtOpc1
+        '
+        Me.RbtOpc1.AutoSize = True
+        Me.RbtOpc1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.RbtOpc1.Location = New System.Drawing.Point(20, 12)
+        Me.RbtOpc1.Name = "RbtOpc1"
+        Me.RbtOpc1.Size = New System.Drawing.Size(91, 25)
+        Me.RbtOpc1.TabIndex = 0
+        Me.RbtOpc1.TabStop = True
+        Me.RbtOpc1.Text = "Opción 1"
+        Me.RbtOpc1.UseVisualStyleBackColor = True
+        '
+        'BtnNum
+        '
+        Me.BtnNum.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnNum.Location = New System.Drawing.Point(69, 284)
+        Me.BtnNum.Name = "BtnNum"
+        Me.BtnNum.Size = New System.Drawing.Size(106, 35)
+        Me.BtnNum.TabIndex = 3
+        Me.BtnNum.Text = "0"
+        Me.BtnNum.UseVisualStyleBackColor = True
+        '
+        'Txt1
+        '
+        Me.Txt1.Location = New System.Drawing.Point(69, 356)
+        Me.Txt1.Name = "Txt1"
+        Me.Txt1.Size = New System.Drawing.Size(100, 23)
+        Me.Txt1.TabIndex = 4
+        '
+        'Txt2
+        '
+        Me.Txt2.Location = New System.Drawing.Point(196, 357)
+        Me.Txt2.Name = "Txt2"
+        Me.Txt2.Size = New System.Drawing.Size(104, 23)
+        Me.Txt2.TabIndex = 4
+        '
+        'BtnTexto
+        '
+        Me.BtnTexto.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.BtnTexto.Location = New System.Drawing.Point(327, 357)
+        Me.BtnTexto.Name = "BtnTexto"
+        Me.BtnTexto.Size = New System.Drawing.Size(127, 24)
+        Me.BtnTexto.TabIndex = 5
+        Me.BtnTexto.Text = "Cambiar nombre"
+        Me.BtnTexto.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BtnTexto)
+        Me.Controls.Add(Me.Txt2)
+        Me.Controls.Add(Me.Txt1)
+        Me.Controls.Add(Me.BtnNum)
+        Me.Controls.Add(Me.PnlRbt)
+        Me.Controls.Add(Me.BtnPulsa)
+        Me.Controls.Add(Me.LblDobleClick)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.PnlRbt.ResumeLayout(False)
+        Me.PnlRbt.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnPulsa As Button
+    Friend WithEvents LblDobleClick As Label
+    Friend WithEvents PnlRbt As Panel
+    Friend WithEvents RbtOpc4 As RadioButton
+    Friend WithEvents RbtOpc3 As RadioButton
+    Friend WithEvents RbtOpc2 As RadioButton
+    Friend WithEvents RbtOpc1 As RadioButton
+    Friend WithEvents BtnNum As Button
+    Friend WithEvents Txt1 As TextBox
+    Friend WithEvents Txt2 As TextBox
+    Friend WithEvents BtnTexto As Button
+End Class
---
+++ Practica01/Práctica1/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub BtnPulsa_Click(sender As Object, e As EventArgs) Handles BtnPulsa.Click
+        Dim MiBoton As Button = sender
+        Static pulsado As Boolean
+        If Not pulsado Then
+            sender.Text = "Pulsado"
+            pulsado = True
+        Else
+            MessageBox.Show("Ya ha sido pulsado", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub LblDobleClick_DoubleClick(sender As Object, e As EventArgs) Handles LblDobleClick.DoubleClick
+        Dim MiLabel As Label = sender
+        MiLabel.Text &= " P"
+    End Sub
+
+    Private Sub RbtOpc1_CheckedChanged(sender As Object, e As EventArgs) Handles _
+        RbtOpc1.Click, RbtOpc2.Click, RbtOpc3.Click, RbtOpc4.Click
+        Dim MiRadio As RadioButton = sender
+        MessageBox.Show(MiRadio.Text & " seleccionado", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub BtnNum_Click(sender As Object, e As EventArgs) Handles BtnNum.Click
+        Dim MiBoton As Button = sender
+        Static Contador As Integer
+        Contador += 1
+        MiBoton.Text = Contador
+    End Sub
+
+    Private Sub BtnTexto_Click(sender As Object, e As EventArgs) Handles BtnTexto.Click
+        Dim Texto As String
+        Texto = Txt1.Text & " " & Txt2.Text
+        Me.Text = Texto
+    End Sub
+End Class
