294617e19d246d30659eced54ce36a5b705fe382
clopezba <200101@iessierradeguara.com>
2021-10-26 10:10:06 +0200

    Constructor - add handler

---
+++ Constructor/Constructor/Form1.vb
@@ -0,0 +1,23 @@
+﻿Imports MisClases
+
+Public Class Form1
+    Private Sub btnAnadir_Click(sender As Object, e As EventArgs) Handles btnAnadir.Click
+        Dim MiTextBox As CajaTexto
+        If pnl.Controls.Count > 9 Then Exit Sub
+        MiTextBox = New CajaTexto
+        MiTextBox.Top = pnl.Controls.Count * (MiTextBox.Height + 5)
+        AddHandler MiTextBox.DoubleClick, AddressOf MiTexto
+        pnl.Controls.Add(MiTextBox)
+        MiTextBox.Name = "TextBox_" & pnl.Controls.Count
+        lblContador.Text = "Nº de TextBox " & pnl.Controls.Count
+    End Sub
+    Private Sub btnQuitar_Click(sender As Object, e As EventArgs) Handles btnQuitar.Click
+        If pnl.Controls.Count = 0 Then Exit Sub
+        pnl.Controls.RemoveAt(pnl.Controls.Count - 1)
+        lblContador.Text = "Nº de TextBox " & pnl.Controls.Count
+    End Sub
+    Private Sub MiTexto(Objeto As Object, Evento As EventArgs)
+        Dim MiTexto As TextBox = Objeto
+        MessageBox.Show("Contenido de: " & MiTexto.Name & " --> " & MiTexto.Text)
+    End Sub
+End Class
---
+++ Constructor/MisClases/CajaTexto.vb
@@ -0,0 +1,15 @@
+﻿'Clase que hereda de otra clase y puedo modificar sus propiedades o funciones
+Public Class CajaTexto
+    Inherits TextBox
+    Public Overrides Property ForeColor As Color 'Al escribir Public Overrides nos muestra los métodos que se pueden sobreescribir
+        Get
+            Dim MiColor As Color = Color.FromArgb(RGB(Rnd() * 255, Rnd() * 255, Rnd() * 255))
+            'Return MyBase.ForeColor
+            'Return Color.DarkRed
+            Return MiColor
+        End Get
+        Set(value As Color)
+            MyBase.ForeColor = value
+        End Set
+    End Property
+End Class
