891214b4e93bae3f0ed101bc51e9e62ff7f495e4
clopezba <200101@iessierradeguara.com>
2021-11-17 14:18:55 +0100

    fich1

---
+++ Ficheros1/Ficheros1/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtTexto = New System.Windows.Forms.TextBox()
+        Me.btnCargar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnCargarVB = New System.Windows.Forms.Button()
+        Me.btnGuardarVB = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtTexto
+        '
+        Me.txtTexto.Location = New System.Drawing.Point(189, 54)
+        Me.txtTexto.Multiline = True
+        Me.txtTexto.Name = "txtTexto"
+        Me.txtTexto.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
+        Me.txtTexto.Size = New System.Drawing.Size(555, 375)
+        Me.txtTexto.TabIndex = 0
+        '
+        'btnCargar
+        '
+        Me.btnCargar.Location = New System.Drawing.Point(24, 19)
+        Me.btnCargar.Name = "btnCargar"
+        Me.btnCargar.Size = New System.Drawing.Size(92, 30)
+        Me.btnCargar.TabIndex = 1
+        Me.btnCargar.Text = "Cargar fichero"
+        Me.btnCargar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(24, 55)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(92, 30)
+        Me.btnGuardar.TabIndex = 1
+        Me.btnGuardar.Text = "Guardar fichero"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBorrar.Location = New System.Drawing.Point(405, 12)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(129, 30)
+        Me.btnBorrar.TabIndex = 1
+        Me.btnBorrar.Text = "Borrar texto"
+        Me.btnBorrar.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnCargar)
+        Me.GroupBox1.Controls.Add(Me.btnGuardar)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 29)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(140, 100)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "StreamReader/Writer"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnCargarVB)
+        Me.GroupBox2.Controls.Add(Me.btnGuardarVB)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 153)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(140, 100)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "VB Files"
+        '
+        'btnCargarVB
+        '
+        Me.btnCargarVB.Location = New System.Drawing.Point(24, 19)
+        Me.btnCargarVB.Name = "btnCargarVB"
+        Me.btnCargarVB.Size = New System.Drawing.Size(92, 30)
+        Me.btnCargarVB.TabIndex = 1
+        Me.btnCargarVB.Text = "Cargar fichero"
+        Me.btnCargarVB.UseVisualStyleBackColor = True
+        '
+        'btnGuardarVB
+        '
+        Me.btnGuardarVB.Location = New System.Drawing.Point(24, 55)
+        Me.btnGuardarVB.Name = "btnGuardarVB"
+        Me.btnGuardarVB.Size = New System.Drawing.Size(92, 30)
+        Me.btnGuardarVB.TabIndex = 1
+        Me.btnGuardarVB.Text = "Guardar fichero"
+        Me.btnGuardarVB.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnBorrar)
+        Me.Controls.Add(Me.txtTexto)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtTexto As TextBox
+    Friend WithEvents btnCargar As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnCargarVB As Button
+    Friend WithEvents btnGuardarVB As Button
+End Class
---
+++ Ficheros1/Ficheros1/Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub btnCargar_Click(sender As Object, e As EventArgs) Handles btnCargar.Click
+        Dim MiDialogOpen As New OpenFileDialog
+        MiDialogOpen.InitialDirectory = My.Application.Info.DirectoryPath
+        MiDialogOpen.ShowDialog()
+
+        Try
+            Dim MiFichero As New StreamReader(MiDialogOpen.FileName)
+            txtTexto.Text = MiFichero.ReadToEnd
+            'Tenemos que cerrar el flujo porque se queda abierto un tiempo
+            MiFichero.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Dim MiDialogGuardar As New SaveFileDialog
+        MiDialogGuardar.InitialDirectory = My.Application.Info.DirectoryPath
+        MiDialogGuardar.ShowDialog()
+
+        Dim MiFichero As New StreamWriter(MiDialogGuardar.FileName)
+        MiFichero.Write(txtTexto.Text)
+        MiFichero.Close()
+    End Sub
+
+    Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+        txtTexto.Clear()
+    End Sub
+
+    Private Sub btnCargarVB_Click(sender As Object, e As EventArgs) Handles btnCargarVB.Click
+        'Da un entero único que será un canal. Sobre ese canal abriremos el fichero
+        Dim Canal As Integer = FreeFile()
+        Dim MiFichero As String = "ficheros.txt"
+        'MiFichero = Dir(MiFichero) 'Devuelve el nombre del  directorio, fichero o ruta 
+        Dim Texto As String = ""
+        Try
+            FileOpen(Canal, MiFichero, OpenMode.Input, OpenAccess.Read) 'Ha cargado el fichero sobre el canal
+            Do While Not EOF(Canal)
+                Texto &= LineInput(Canal) & vbCrLf
+            Loop
+            txtTexto.Text = Texto
+            FileClose(Canal)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+
+
+    End Sub
+
+    Private Sub btnGuardarVB_Click(sender As Object, e As EventArgs) Handles btnGuardarVB.Click
+        Dim Canal As Integer = FreeFile()
+        Dim MiFichero As String = "fichero.txt"
+        FileOpen(Canal, MiFichero, OpenMode.Output, OpenAccess.Write)
+        'WriteLine(Canal, txtTexto.Text) 'Añade comillas al inicio y fin de texto
+        Write(Canal, txtTexto.Text) 'Nos pone además un punto al final de la línea
+        FileClose(Canal)
+    End Sub
+End Class
