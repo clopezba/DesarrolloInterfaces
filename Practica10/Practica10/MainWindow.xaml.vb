aed17d1dc723d7913fd6d84848d074dfcc7ead50
Cristina <200101@iessierradeguara.com>
2021-11-25 22:45:44 +0100

    fin p10

--- Practica10/Practica10/MainWindow.xaml.vb
+++ Practica10/Practica10/MainWindow.xaml.vb
@@ -14,8 +14,7 @@
     End Sub
 
     'Introducir items a la lista al presionar Enter evitando repetir o introducir espacio en blanco
-    Private Sub txtIzq_KeyPress(sender As Object, e As KeyEventArgs
-                                )
+    Private Sub txtIzq_KeyPress(sender As Object, e As KeyEventArgs)
         Dim MiTexto As TextBox = sender
         Dim MiListaEntrada As ListBox = MiTexto.Tag
         If MiTexto.Text.Trim = "" Or MiListaEntrada.Items.IndexOf(MiTexto.Text) <> -1 Then
@@ -39,7 +38,7 @@
     'Mover un item de una lista a otra
     Private Sub IntercambioListas()
         Do While MiListaOrigen.SelectedItems.Count
-            If MiListaDestino.Items.IndexOf(MiListaOrigen.SelectedItems(0)) <> -1 Then Exit Sub
+          If MiListaDestino.Items.IndexOf(MiListaOrigen.SelectedItems(0)) <> -1 Then Exit Sub
             MiListaDestino.Items.Add(MiListaOrigen.SelectedItems(0))
             MiListaOrigen.Items.Remove(MiListaOrigen.SelectedItems(0))
         Loop
@@ -62,11 +61,53 @@
                 MiListaOrigen = listaDer
                 MiListaDestino = listaCen
         End Select
-        'If MiListaOrigen.SelectedItems.Count = 0 Then
-        '    For i As Integer = 0 To MiListaOrigen.Items.Count - 1
-        '        MiListaOrigen.SelectedItem(i)
-        '    Next i
-        'End If
+        If MiListaOrigen.SelectedItems.Count = 0 Then
+            MiListaOrigen.SelectAll()
+        End If
         IntercambioListas()
     End Sub
+    'Al hacer doble click sobre la lista, se pasan los items seleccionados a la lista siguiente
+    Private Sub ltbxIzq_DoubleClick(sender As Object, e As MouseButtonEventArgs) Handles listaCen.MouseDoubleClick, listaDer.MouseDoubleClick, listaIzq.MouseDoubleClick
+        Dim MiLista As ListBox = sender
+        If MiLista.SelectedItems.Count = 0 Then Exit Sub
+        Select Case MiLista.Name
+            Case "listaIzq"
+                MiListaOrigen = listaIzq
+                MiListaDestino = listaCen
+            Case "listaCen"
+                MiListaOrigen = listaCen
+                MiListaDestino = listaDer
+            Case "listaDer"
+                MiListaOrigen = listaDer
+                MiListaDestino = listaIzq
+        End Select
+        IntercambioListas()
+    End Sub
+
+    'Drag&Drop Al pulsar una lista sin soltar y arrastrar hacia otra lista, se pasan los items seleccionados
+    Private Sub ltbxIzq_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles listaIzq.MouseDown, listaCen.MouseDown, listaDer.MouseDown
+        MiListaOrigen = sender
+        Select Case MiListaOrigen.Name
+            Case "listaIzq"
+                MiListaDestino = listaCen
+            Case "listaCen"
+                MiListaDestino = listaDer
+            Case "listaDer"
+                MiListaDestino = listaIzq
+        End Select
+        If e.ClickCount = 2 Then
+            IntercambioListas()
+            Exit Sub
+        Else
+            If MiListaOrigen.SelectedItems.Count > 0 And e.LeftButton.Pressed Then
+                DragDrop.DoDragDrop(MiListaOrigen, MiListaOrigen.SelectedItem.ToString, DragDropEffects.Move)
+            End If
+        End If
+    End Sub
+
+    Private Sub ltbxIzq_DragDrop(sender As Object, e As DragEventArgs) Handles listaIzq.Drop, listaCen.Drop, listaDer.Drop
+        MiListaDestino = sender
+        IntercambioListas()
+    End Sub
+
 End Class
