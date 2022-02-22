7fde6e92585f37f6bd32ddcc28fd9565f02fc6da
clopezba <200101@iessierradeguara.com>
2022-02-22 10:18:50 +0100

    ..

--- AlmacenMateriales/AlmacenMateriales/CrearMaterial.vb
+++ AlmacenMateriales/AlmacenMateriales/CrearMaterial.vb
@@ -1,13 +1,61 @@
 ﻿Imports System.ComponentModel
+Imports System.Data.SqlClient
+Imports System.IO
 
 Public Class crearMaterial
+
+    Dim conexion As SqlConnection
+    Private Sub crearMaterial_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim direcActual As String = Directory.GetCurrentDirectory()
+        Dim directorio As String = Directory.GetParent(Directory.GetParent(direcActual).ToString()).ToString()
+
+        'Establecer conexión con BD
+        Dim conexionString As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & directorio & "\AlmacenMaterialesDB.mdf;Integrated Security=True;"
+        conexion = New SqlConnection(conexionString)
+        Try
+            conexion.Open()
+            Console.WriteLine("Conexión establecida")
+        Catch ex As Exception
+            Console.WriteLine("No se ha podido conectar. Error: " + ex.Message)
+        End Try
+
+        obtenerNumeroMaterial()
+    End Sub
+
+    'Recibe de la BD el número del último material registrado
+    Private Sub obtenerNumeroMaterial()
+        Dim ultimo As Integer = 0
+        Dim consultaNum As String = "select IDENT_CURRENT('Materiales') as ultimoMat"
+        Dim comando As SqlCommand = New SqlCommand(consultaNum, conexion)
+        Dim num As SqlDataReader = comando.ExecuteReader()
+
+        If num.Read() = True Then
+            Try
+                ultimo = num("ultimoMat")
+            Catch ex As Exception
+                Console.WriteLine(ex.Message)
+            End Try
+            txtnum_mat.Text = Format(ultimo + 1, "00000000")
+        End If
+        num.Close()
+    End Sub
+
+    Private Sub cmbcat_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbcat.SelectedIndexChanged
+        Select Case cmbcat.SelectedIndex
+            Case 0
+
+        End Select
+
+    End Sub
+
+
 
     Private Sub crearMaterial_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
         If modificarMaterial.Visible = False And listarMateriales.Visible = False And eliminarMaterial.Visible = False Then
             inicio.Visible = True
         End If
     End Sub
-
+    '+++++++++FUNCIONES MENÚ E ICONOS+++++++++++
     Private Sub icon_inicio_Click(sender As Object, e As EventArgs) Handles icon_inicio.Click
         inicio.Visible = True
         Me.Close()
@@ -38,4 +86,6 @@ Public Class crearMaterial
             eliminarMaterial.ShowDialog()
         End If
     End Sub
+
+
 End Class
--- AlmacenMateriales/AlmacenMateriales/Inicio.vb
+++ AlmacenMateriales/AlmacenMateriales/Inicio.vb
@@ -1,20 +1,24 @@
 ﻿Public Class inicio
     Private Sub crear_Click(sender As Object, e As EventArgs) Handles btn_crear.Click, icon_crear.Click, menu_crear.Click
-        Me.Hide()
-        crearMaterial.ShowDialog()
+        Dim crear As New crearMaterial
+        crear.Show()
+        Me.Close()
     End Sub
 
     Private Sub modif_Click(sender As Object, e As EventArgs) Handles btn_modif.Click, icon_modificar.Click, menu_modif.Click
-        Me.Hide()
-        modificarMaterial.ShowDialog()
+        Dim modif As New modificarMaterial
+        modif.Show()
+        Me.Close()
     End Sub
     Private Sub listar_Click(sender As Object, e As EventArgs) Handles btn_listar.Click, icon_listar.Click, menu_listar.Click
-        Me.Hide()
-        listarMateriales.ShowDialog()
+        Dim listar As New listarMateriales
+        listar.Show()
+        Me.Close()
     End Sub
     Private Sub eliminar_Click(sender As Object, e As EventArgs) Handles btn_eliminar.Click, icon_eliminar.Click, menu_eliminar.Click
-        Me.Hide()
-        eliminarMaterial.ShowDialog()
+        Dim eliminar As New eliminarMaterial
+        eliminar.Show()
+        Me.Close()
     End Sub
 
 
